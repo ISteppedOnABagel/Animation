788bbead9300a9647257c0f81a8f418f26c751af
Celeste Garcia <103707181+ISteppedOnABagel@users.noreply.github.com>
2025-10-19 19:38:43 +0000

    Refactor animation logic and update button onclick handlers to use animateFade function

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-const button = document.getElementById("button");
-const element = document.getElementById("myImage");
+const image = document.getElementById("myImage");
+const demo1 = document.getElementById("demo1");
+const demo2 = document.getElementById("demo2");
+const demo3 = document.getElementById("demo3");
 
-button.onclick = fade;
 
-function fade() {
-  element.classList.toggle("animate");
+function animateFade(text) {
+  image.classList.remove("animate");
+  setTimeout(() => {
+    image.src = "Barry-" + text + (text == "Happy" ? ".png" : ".webp");
+    image.classList.add("animate");
+  }, 100);
 }
